fe618e354e097e35f4c063011dc244e34b112c9d
anthonycsilva <anthonycsilva@github.com>
2021-03-30 09:21:22 -0300

    exercicio 1, fodase o 2

--- semana1/ex01.py
+++ semana1/ex01.py
@@ -1,1 +1,50 @@
-print('Olá Mundo')
+#PRIMEIRO PEPINO: DA UM JEITO DE ACHAR O MELHOR JOGADOR DA COPA
+#CADA ARTILHEIO VAI RECEBER UMA ESPAÇO NA MATRIZ COM O NOME DELE, RODADA, NUMERO DE GOLS.
+#SÓ QUE EM RODADA VAI ATÉ EM QUAL RODADA ELE JOGOU POR ULTIMO PODENDO SER, CLASSIFICATÓRIA, OITAVAS, QUARTAS, SEMI, FINAL
+
+#QUEM JOGOU CLASSIFICATÓRIA JOGOU 3 JOGOS, OITAVAS 4, QUARTAS 5, SEMI 6, FINAL 7
+
+
+#SEGUNDO PEPINO:FUNÇÕES MEDIAGOLS
+#UMA FUNÇÃO QUE DADA A MATRIZ DE ARTILHEIROS, CALCULA A MATRIZ COM A MÉDIA DE GOLS DURANTE A COPA
+
+#TERCEIRO PEPINO:FUNÇÃO CHUTEIRA DE OURO É SUA MAIN, AONDE AS OUTRAS DEVEM SER CHAMADAS, DEVE RETORNAR O JOGADOR COM A MAIOR MÉDIA DO CAMP E QUE TENHA IDO ALEM DAS CLASSIFICATÓRIAS, RETORNAR O NOME E A MÉDIA DE GOLS
+
+
+def artilheiros():
+    jogadores = [
+                ['Anthony Perna Mansa',5,18],
+                ['Samarinha Pé elétrico',6,10],
+                ['Diego Beionce',4,12],
+                ['Voldollar',7,14],
+                ['Shrek Ninja',3,32],
+                ]
+
+    return jogadores
+
+def mediaGolsJogadores(matrizJogadores):
+    gols = list()
+    classificados = list()
+    for jogadores in range(0, len(matrizJogadores)):
+        if matrizJogadores[jogadores][1] > 3:
+            gols.append(matrizJogadores[jogadores][2])
+            classificados.append(matrizJogadores[jogadores])
+    return [(sum(gols)/len(matrizJogadores)), classificados]
+
+
+def chuteiraOuro():
+    dados = mediaGolsJogadores(artilheiros())
+    media = dados[0]
+    o_mais_pika = list()
+    for jogadores in range(0, len(dados[1])):
+        if len(o_mais_pika) == 0:
+            o_mais_pika.append(dados[1][jogadores])
+        else:
+            if dados[1][jogadores][2] >= o_mais_pika[0][2]:
+                o_mais_pika[0] = dados[1][jogadores]
+    mediaGols = o_mais_pika[0][2]/o_mais_pika[0][1]
+    print(f'O jogador, {o_mais_pika[0][0]}, recebeu a Chuteira de Ouro com uma Média de {mediaGols} gols por partida!')
+    return True
+
+
+chuteiraOuro()
--- semana1/ex02.py
+++ semana1/ex02.py
@@ -1,1 +1,46 @@
-print('Olá Mundo')
+#PRIMEIRO PEPINO, CRIAR UMA MATRIZ COM AS TABLEAS PARA O BINGO
+#NUMEROS RANDOMS DE 1 A 60, SENDO QUE VOU FAZER ELAS DISTRIBUIDAS 2 X 3 ( 2 LINHAS PARA 3 COLUNAS )
+import random
+##[0 for linha in range(1,4)],[0 for coluna in range(1,4)]
+cartelas = list()
+def criaCartela(quantidade):
+    i = 0
+    while i < quantidade:
+        cartelas.append([[0 for c in range(1,4)],[0 for l in range(1,4)]])
+        i+=1
+    return True
+
+
+def adicionaNumero(cartelas):
+    for cartela in range(0,len(cartelas)):
+        for coluna in range(0,2):
+            for linha in range(0,3):
+                cartelas[cartela][coluna][linha] = random.randint(1,61)
+
+
+def sorteiaNumero():
+    numeroSorteado = random.randint(1,61)
+    return numeroSorteado
+
+
+def verificaSorteio(cartelas):
+    
+    while True:
+        numero = sorteiaNumero()
+        for cartela in range(0,len(cartelas)):
+            for coluna in range(0,2):
+                for linha in range(0,3):
+                    if cartelas[cartela][coluna][linha] == numero:
+                        print(cartelas[cartela])
+                        print(numero)
+                        return cartela+1
+                    else: 
+                        print('achei não mano')
+    
+    
+
+                        
+                     
+criaCartela(5)
+adicionaNumero(cartelas)
+print(verificaSorteio(cartelas))
